bb549a14bcf0fe7a47662c56f24b5ecabd406c8d
caubetotbunggg <166147553+caubetotbunggg@users.noreply.github.com>
2024-09-27 17:18:54 +0700

    twofirst

---
+++ Exercises/ex1.java
@@ -0,0 +1,34 @@
+package Exercises;
+
+import java.lang.Math;
+import java.util.Scanner;
+
+public class ex1{
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        double sum = num1 + num2;
+        double difference = Math.abs(num1 - num2);
+        double product = num1 * num2;
+
+        if (num2 != 0) {
+            double quotient = num1 / num2;
+            System.out.printf("Quotient: %.2f\n", quotient);
+        } else {
+            System.out.println("Quotient: Cannot divide by zero");
+        }
+
+        System.out.printf("Sum: %.2f\n", sum);
+        System.out.printf("Difference: %.2f\n", difference);
+        System.out.printf("Product: %.2f\n", product);
+
+        scanner.close();
+    }
+}
---
+++ Exercises/ex2.java
@@ -0,0 +1,108 @@
+package Exercises;
+
+import java.util.Scanner;
+
+public class ex2 {
+        public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Choose the type of equation to solve: ");
+        System.out.println("1. First degree equation with one variable (ax + b = 0) ");
+        System.out.println("2. First degree equations with two variables");
+        System.out.println("3. Second-degree equation with one variable ");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                solveCaseOne(scanner);
+                break;
+            case 2:
+                solveCaseTwo(scanner);
+                break;
+            case 3:
+                solveCaseThree(scanner);
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
+        scanner.close();
+    }
+    
+    private static void solveCaseOne(Scanner scanner){
+            System.out.println("input a: ");
+            double num1 = scanner.nextDouble();
+
+        if(num1 == 0){
+            System.out.println("a cant be 0 !");
+        }else{
+            System.out.println("input b: ");
+            double num2 = scanner.nextDouble();
+            double ans = (-num2) / num1;
+            System.out.printf("x: %.2f", ans);
+        }    
+    }
+
+
+    private static void solveCaseTwo(Scanner scanner){
+        System.out.print("Input coefficient a11: ");
+        double a11 = scanner.nextDouble();
+        System.out.print("Input coefficient a12: ");
+        double a12 = scanner.nextDouble();
+        System.out.print("Input coefficient b1: ");
+        double b1 = scanner.nextDouble();
+        System.out.print("Input coefficient a21: ");
+        double a21 = scanner.nextDouble();
+        System.out.print("Input coefficient a22: ");
+        double a22 = scanner.nextDouble();
+        System.out.print("Input coefficient b2: ");
+        double b2 = scanner.nextDouble();
+
+        double D1 = b1 * a22 - b2 * a12;
+        double D2 = a11 * b2 - a21 * b1;
+        double D = a11 * a22 - a21 * a12;
+
+        if (D == 0) {
+            if (D1 == 0 && D2 == 0) {
+                System.out.println("Infinite solutions.");
+            } else {
+                System.out.println("No solution.");
+            }
+        } else {
+            double x1 = D1 / D;
+            double x2 = D2 / D;
+            System.out.println("x1: " + x1 + ", x2: " + x2);
+        }
+
+    }
+
+
+    private static void solveCaseThree(Scanner scanner) {
+        System.out.print("Input coefficient a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Input coefficient b: ");
+        double b = scanner.nextDouble();
+        System.out.print("Input coefficient c: ");
+        double c = scanner.nextDouble();
+
+        if (a == 0) {
+            System.out.println("a cant be 0.");
+        } else {
+            double delta = b * b - 4 * a * c;
+            if (delta > 0) {
+                double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+                System.out.println("x1 = " + x1 + ", x2 = " + x2);
+            } else if (delta == 0) {
+                double x = -b / (2 * a);
+                System.out.println("x = " + x);
+            } else {
+                System.out.println("The equation has no real roots");
+            }
+        }
+    }
+}
+
+
+
+
