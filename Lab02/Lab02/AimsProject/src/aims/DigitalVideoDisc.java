42bae8a1659ae96969aba1f9327101c901b587de
caubetotbunggg <166147553+caubetotbunggg@users.noreply.github.com>
2024-10-27 00:24:31 +0700

    remove setter from class diagram

---
+++ Lab02/Lab02/AimsProject/src/aims/DigitalVideoDisc.java
@@ -0,0 +1,62 @@
+package aims;
+
+public class DigitalVideoDisc {
+	private String title;
+	private String category;
+	private String director;
+	private int length;
+	private float cost;
+	
+	// Create DVD 
+	
+	public DigitalVideoDisc(String title) {
+		super();
+		this.title = title;
+	}
+	public DigitalVideoDisc(String title, String category, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+	}
+	public DigitalVideoDisc(String title, String category, String director, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.cost = cost;
+	}
+	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.length = length;
+		this.cost = cost;
+	}
+	
+    
+    // Only generate getter in this case
+    
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getCategory() {
+		return category;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public float getCost() {
+		return cost;
+	}
+
+	
+}
