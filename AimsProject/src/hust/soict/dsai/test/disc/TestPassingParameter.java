9851339d35be8411653daacf6fa11323ad32df0e
caubetotbunggg <166147553+caubetotbunggg@users.noreply.github.com>
2024-11-25 23:08:04 +0700

    update usecase, diagram class and report file

--- AimsProject/src/hust/soict/dsai/test/disc/TestPassingParameter.java
+++ AimsProject/src/hust/soict/dsai/test/disc/TestPassingParameter.java
@@ -15,10 +15,16 @@ public class TestPassingParameter {
         System.out.println("jungle dvd title: " + jungleDVD.getTitle());
     }
 
-    public static void swap(DigitalVideoDisc o1, DigitalVideoDisc o2) {
-        String oldTitle = o1.getTitle();
-        o1.setTitle(o2.getTitle());
-        o2.setTitle(oldTitle);
+    // public static void swap(DigitalVideoDisc o1, DigitalVideoDisc o2) {
+    //     String oldTitle = o1.getTitle();
+    //     o1.setTitle(o2.getTitle());
+    //     o2.setTitle(oldTitle);
+    // }
+
+    public static void swap(Object o1, Object o2) {
+        Object tmp = o1;
+        o1 = o2;
+        o2 = tmp;
     }
 
     public static void changeTitle(DigitalVideoDisc dvd, String title) {
